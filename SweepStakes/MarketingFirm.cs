e7828a7292962da5d6331baf1989befe0db7db06
alexRCallies <59710714+alexRCallies@users.noreply.github.com>
2020-01-31 13:03:49 -0600

    added the marketing firm class with empty method

---
+++ SweepStakes/MarketingFirm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepStakes
+{
+    class MarketingFirm : ISweepStakeManager
+    {
+        List<ISweepStakeManager> manager = new List<ISweepStakeManager>();
+
+        public MarketingFirm(ISweepStakeManager manager)
+        {
+
+        }
+        public void CreateSweepStakes()
+        {
+
+        }
+    }
+}
